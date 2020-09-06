bdb36e1e2c9c8493016a3cc8374c6866f5ea0b98
muyoul.lee <muyoul.lee@bespinglobal.com>
2020-09-07 07:21:44 +0900

    build name 변경

--- valved.bat
+++ valved.bat
@@ -1,2 +1,2 @@
 @echo off
-docker run -it -v "C:\Users\lemy0\.kube":/mnt/kube -v %cd%/:/mnt/home -v /var/run/docker.sock:/var/run/docker.sock -w /mnt/home lemy0715/valve-on-docker /bin/bash -c "cp /mnt/kube/config /root/.kube/config; /root/.local/bin/valve %*"
+docker run -it -v "C:\Users\lemy0\.kube":/mnt/kube -v %cd%/:/mnt/home -v /var/run/docker.sock:/var/run/docker.sock -w /mnt/home lemy0715/valved /bin/bash -c "cp /mnt/kube/config /root/.kube/config; /root/.local/bin/valve %*"
